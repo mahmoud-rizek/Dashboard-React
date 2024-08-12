223798f45e57024b9b593a0ad1c8f9a30c6a22c2
Mahmoud Abd El-Razek <105005452+mahmoud-rizek@users.noreply.github.com>
2024-08-12 19:16:14 +0300

    ..

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Admin Dashboard using React + Tailwind
 
-![React Admin Dashboard](F:\Projects\React\Dashboard-React\346431833-a0478564-2f60-4451-b907-2bf9dc770b39.png)
+![React Admin Dashboard](F:\Projects\React\Dashboard-React\readme-imge.png)
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { Sidebar } from "./components/Sidebar";
+import { Navbar } from "./components/Navbar";
+import { Dashboard } from "./components/Dashboard";
+import ThemeContextProvider from "./context/ThemeContexProvider";
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <ThemeContextProvider>
+      <div className="flex">
+        <Sidebar />
+        <div
+          className="grow ml-16 md:ml-64 h-full lg:h-screen bg-gray-100 text-gray-900
+      dark:bg-gray-900 dark:text-white"
+        >
+          <Navbar />
+          <div>
+            <Dashboard />
+          </div>
+        </div>
+      </div>
+    </ThemeContextProvider>
   );
 }
 
